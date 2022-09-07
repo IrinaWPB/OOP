9ac5b2b7db6bc5415ec6fb91813512e064b88f2e
IrinaJ <irina.ul87@gmail.com>
2022-09-07 13:12:02 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+class Vehicle {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    honk() {
+        return 'Beep';
+    }
+    toString() {
+        return `The vehicle is a ${this.make} ${this.model} from ${this.year}`;
+    }
+}
+
+class Car extends Vehicle {
+    constructor(make, model, year, numWheels){
+        super(make, model, year);
+        this.numWheels = numWheels;
+    }
+}
+
+class Motorcycle extends Vehicle {
+    constructor(make, model, year){
+        super(make, model, year);
+        this.numWheels = 2;
+    }
+    revEngine() {
+        return 'VROOM';
+    }
+}
+
+class Garage {
+    constructor(capacity) {
+        this.vehicles = [];
+        this.capacity = capacity;  
+    }
+    add(vehicle) {
+        if (this.vehicles.length < this.capacity) {
+            if(vehicle instanceof Vehicle) {
+                this.vehicles.push(vehicle);
+                return `Vehicle added`;          
+            } else {
+                return `Only vehicles are allowed in here!`;
+            }
+        }
+        return `Sorry, we're full.`;
+    }
+}
